87d9fdb2c7e545f22e6e38b6d625736a04333c41
lahiruasiri <lahiruasiri1@gmail.com>
2025-11-20 22:44:46 +0530

    Create firebase.ts

---
+++ src/api/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FIREBASE_CONFIG } from '../constants/config';
+
+// Initialize Firebase App
+const app = initializeApp(FIREBASE_CONFIG);
+
+// Initialize Auth with AsyncStorage persistence
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+// Initialize Firestore
+export const db = getFirestore(app);
