57ba821269e5e65fda7e8d39f1a6f44824e80b97
lahiruasiri <lahiruasiri1@gmail.com>
2026-03-08 10:55:18 +0530

    Update ChatScreen.tsx

--- src/screens/user/ChatScreen.tsx
+++ src/screens/user/ChatScreen.tsx
@@ -32,8 +32,8 @@ interface Message {
 }
 
 const SUGGESTIONS = [
-    "Predict ETA for 177",
-    "Will 177 be crowded tmrw 9am?",
+    "Predict ETA for 400/4",
+    "Will 400/4 be crowded tmrw 9am?",
     "Find bus to Malabe",
 ];
 
